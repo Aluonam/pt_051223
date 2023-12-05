eb8e7b42981778be5b7c8c929ff6d901090def6b
Paula Cano <paulacanoroman@gmail.com>
2023-12-05 13:02:36 +0100

    feat: api call and show data in console

--- components/ApiCall.jsx
+++ components/ApiCall.jsx
@@ -4,14 +4,16 @@ const ApiCall = () => {
 
     const [dataAPI, setDataAPI] = useState([])
 
-    const apiConsume = ()=>{
-
+    const apiConsume = async ()=>{
+        try{
+            const url = await fetch(`https://reqres.in/api/users?page=2`);
+            const data = await url.json();
+            console.log(data.data)
+        }catch(error){error,"error detected"}
     }
-
+    apiConsume()
   return (
     <>
-    <input></input>
-    <br></br>
     modal
     </>
   )
